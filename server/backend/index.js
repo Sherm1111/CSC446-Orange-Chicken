1a5af649bfb59368be4245481fbbf9491a6e6ea4
Jacob Roberts <johnboyjacob1@gmail.com>
2022-12-15 19:59:29 -0600

    adding login route

--- server/backend/index.js
+++ server/backend/index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const mysql = require("mysql2");
+const { createHash } = require('crypto');
 
 
 const PORT = String(process.env.PORT);
@@ -8,6 +9,7 @@ const MYSQLHOST = String(process.env.MYSQLHOST);
 const MYSQLUSER = String(process.env.MYSQLUSER);
 const MYSQLPASS = String(process.env.MYSQLPASS);
 const SQL = "SELECT * FROM users;"
+
 
 const app = express();
 app.use(express.json());
@@ -35,6 +37,34 @@ app.get("/query", function (request, response) {
     }
   });
 })
+
+app.post("/login", function (request, response) {
+  //recieves the username and password
+  var data = request.body;
+  //hashes password
+  hashpass = createHash('sha256').update(data['password']).digest('hex');
+  //makes username something usable for later in code
+  user = "'"+data["username"]+"'";
+  //query username and passwords from sql database, only if the username is the username input 
+  const USERINFO = "SELECT username,password FROM users WHERE username = " + user +";"
+  connection.query(USERINFO, [true], (error, results, fields) => {
+    if (error) {
+      //database error
+      console.error(error.message);
+      response.status(500).send("database error");
+      //if the input password matches input username password from query
+    } else if (hashpass == results[0]['password']){
+      //send 200 OK
+      console.log("200 OK");
+      response.status(200).send("OK")
+    } else {
+      //if username/password are not in database in correct order prevent login
+      console.log("UNAUTHORIZED");
+      response.status(400).send("UNAUTHORIZED");
+    }
+  });
+})
+
 
 
 app.listen(PORT, HOST);
--- server/frontend/common.js
+++ server/frontend/common.js
@@ -1,5 +1,29 @@
 var parsedUrl = new URL(window.location.href);
 
+
+function login(usr,passwd) {
+    //post request
+    fetch("http://" + parsedUrl.host + "/login", {
+        method: "POST",
+        //specify what is being sent (json)
+        headers: {'Content-Type': 'application/json'},
+        //username and password in json format (dictionary)
+        body: JSON.stringify({username:usr.value, password:passwd.value}),
+    })
+    .then(function(response){
+        //prints to console the status
+        console.log(response.status)
+        //if status is OK move to query page
+        if(response.status == 200){
+            window.location.href = "/query.html";
+        }
+    })
+    //catch errors
+    .catch((err) => {
+        console.log(err);
+    })
+}
+
 function query() {
     fetch("http://" + parsedUrl.host + "/query", {
         method: "GET",

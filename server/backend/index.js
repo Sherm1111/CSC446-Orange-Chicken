43fcb0a914b4948979443ae0e0bb08856722e354
Jacob Roberts <johnboyjacob1@gmail.com>
2023-01-15 17:06:22 -0600

    introduces bcrypt and jwt adding salts to password given by user and generating a token during the login process to be used on the query page.

--- server/backend/index.js
+++ server/backend/index.js
@@ -1,7 +1,8 @@
 const express = require("express");
 const mysql = require("mysql2");
 const { createHash } = require('crypto');
-
+const jwt = require('jsonwebtoken');
+const bc = require('bcrypt');
 
 const PORT = String(process.env.PORT);
 const HOST = String(process.env.HOST);
@@ -26,42 +27,64 @@ let connection = mysql.createConnection({
 app.use("/", express.static("frontend"));
 
 
-app.get("/query", function (request, response) {
-  connection.query(SQL, [true], (error, results, fields) => {
-    if (error) {
-      console.error(error.message);
-      response.status(500).send("database error");
-    } else {
-      console.log(results);
-      response.send(results);
-    }
-  });
+app.post("/query", async (request, response) => {
+  //get the jwt
+  var data = request.body;
+  //verify
+  try{
+    jwt.verify(data[1],'secret')
+    console.log(jwt.verify(data[1],'secret'))
+    connection.query(SQL, [true], (error, results, fields) => {
+      if (error) {
+        console.error(error.message);
+        response.status(500).send("database error");
+      } else {
+        //console.log(results);
+        response.send(results);
+      }
+    });
+  }catch{
+    response.send("401")
+  }
 })
 
-app.post("/login", function (request, response) {
+app.post("/login", async (request, response) => {
   //recieves the username and password
   var data = request.body;
-  // hashes password
-  hashpass = createHash('sha256').update(data['password']).digest('hex');
+  //salt
+  const salt = await bc.genSalt();
+  // hashes password gotten from user
+  const hashpass = createHash('sha256').update(data['password']).digest('hex');
+  //hashes password again using bcrypt
+  const hashpass1 = await bc.hash(hashpass, salt);
   //makes username something usable for later in code
   user = "'"+data["username"]+"'";
   //query username and passwords from sql database, only if the username is the username input 
-  const USERINFO = "SELECT username,password FROM users WHERE username = " + user +";"
-  connection.query(USERINFO, [true], (error, results, fields) => {
-    if (error) {
-      //database error
-      console.error(error.message);
-      response.status(500).send("database error");
-      //if the input password matches input username password from query
-    } else if ("'" + data['password'] + "'" == ''){
-      //  if password is blank
-      response.status(400).send("UNAUTHORIZED");
-    } else if (hashpass == results[0]['password']){
-      //send 200 OK
-      console.log("200 OK");
-      response.status(200).send("OK")
-    } else {
-      //if username/password are not in database in correct order prevent login
+  const USERINFO = "SELECT username,password,role FROM users WHERE username = " + user +";"
+  connection.query(USERINFO, [true], async (error, results, fields) => {
+    try{
+      if (error) {
+        //database error
+        console.error(error.message);
+        response.status(500).send("database error");
+        //if the input password matches input username password from query
+      } else if (data['password'] == ''){
+        //  if password is blank
+        response.status(400).send("UNAUTHORIZED");
+        //compares hashed password in database with hashed and salted password given from user.
+      } else if (await bc.compare(results[0]['password'], hashpass1)){
+        //create token
+        var token = jwt.sign(results[0], "secret", {expiresIn:"30s"})
+        //send 200 OK
+        console.log("200 OK")
+        //send token to common.js
+        response.send(token)
+        
+      } else {
+        //if username/password are not in database in correct order prevent login
+        response.status(400).send("UNAUTHORIZED");
+      }
+    } catch (error){
       console.log("UNAUTHORIZED");
       response.status(400).send("UNAUTHORIZED");
     }
--- server/frontend/common.js
+++ server/frontend/common.js
@@ -1,6 +1,12 @@
 var parsedUrl = new URL(window.location.href);
 
 
+//logout button
+function logout(){
+    window.location.href = '/'
+}
+
+
 function login(usr,passwd) {
     //post request
     fetch("http://" + parsedUrl.host + "/login", {
@@ -8,18 +14,26 @@ function login(usr,passwd) {
         //specify what is being sent (json)
         headers: {'Content-Type': 'application/json'},
         //username and password in json format (dictionary)
-        body: JSON.stringify({username:usr.value, password:passwd.value}),
+        body: JSON.stringify({username:usr.value, password:passwd.value})
     })
-    .then(function(response){
+    .then(async (response) => {
+        //get jwt token
+        token = await response.text()
+        //query string
+        const usp = new URLSearchParams('token='+token)
+        const querystr = usp.toString()
         //prints to console the status
         console.log(response.status)
         //if status is OK move to query page
         if(response.status == 200){
-            window.location.href = "/query.html";
+            window.location.href = "/query.html?"+querystr;
         }
         else{
             alert("username or password does not match");
         }
+    })
+    .then((data) => {
+        console.log()
     })
     //catch errors
     .catch((err) => {
@@ -28,13 +42,27 @@ function login(usr,passwd) {
 }
 
 function query() {
+    //get token from URL
+    const url = window.location.href
+    const searchParams = new URL(url).searchParams
+    const querystring = new URLSearchParams(searchParams)
+    const tokenarr = Array.from(querystring)
     fetch("http://" + parsedUrl.host + "/query", {
-        method: "GET",
-        mode: "no-cors",
+        method: "POST",
+        headers: {'Content-Type': 'application/json'},
+        body: JSON.stringify(tokenarr[0])
     })
-    .then((resp) => resp.text())
+    .then((resp) =>
+        resp.text(),
+    
+    )
     .then((data) => {
         document.getElementById("response").innerHTML = data;
+        //if api sends 401 message go back to login page
+        if(data == "401"){
+            window.location.href = "/"
+        }
+
     })
     .catch((err) => {
         console.log(err);

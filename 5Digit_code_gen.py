5c66f2a893464f69e21d2e92fdac366696cb8f69
landon tomkins <landontomkins@gmail.com>
2023-01-27 10:29:37 -0600

    added python file for generating 5digit code

---
+++ 5Digit_code_gen.py
@@ -0,0 +1,21 @@
+import time
+import hashlib
+
+
+def generate_5Digit_code():
+    secret_key = "secret".encode()
+
+    current_time = time.time()
+    rounded_time_30sec = str(current_time - (current_time % 30)).encode()
+
+    data = rounded_time_30sec + secret_key
+
+
+    hashlib_object = hashlib.sha256()
+    hashlib_object.update(data)
+    hashed_code = hashlib_object.hexdigest()
+
+    return hashed_code[0:5]
+
+code = generate_5Digit_code()
+print(code)
